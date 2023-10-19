df8921454298e03943cf768e2c4a392d60cccf96
Caleb <calebagaro7@gmail.com>
2023-10-19 11:40:13 -0700

    Code now debugging now

---
+++ assembler/Assembler.java
@@ -0,0 +1,119 @@
+package assembler;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+public class Assembler 
+{
+    private File code;
+    private BufferedWriter write;
+    private Code encoder;
+    private SymbolTable table;
+
+    public Assembler(File source, File target) throws IOException
+    {
+        this.code = source;
+        FileWriter fileWrite = new FileWriter(target);
+        this.write = new BufferedWriter(fileWrite);
+        this.encoder = new Code();
+        this.table = new SymbolTable();
+    }
+    
+    public void translate() throws IOException
+    {
+        this.recordLabel();
+        this.parse();
+    }
+
+    private void recordLabel() throws IOException
+    {
+        Parser parser = new Parser(this.code);
+        while(parser.moreCommands())
+        {
+            parser.advance();
+            CommandType command = parser.commandType();
+            if(command.equals(CommandType.L_COMMAND))
+            {
+                String symbol = parser.symbol();
+                int address = this.table.getPAddress();
+                this.table.addEntry(symbol, address);
+            }
+            else
+            {
+                this.table.incrementPAddress();
+            }
+        }
+        parser.close();
+    }
+
+    private void parse() throws IOException
+    {
+        Parser parser = new Parser(this.code);
+        while(parser.moreCommands())
+        {
+            parser.advance();
+            CommandType command = parser.commandType();
+            String instruction = null;
+
+            if(command.equals(CommandType.A_COMMAND))
+            {
+                String symbol = parser.symbol();
+                Character first = symbol.charAt(0);
+                boolean isSymbol = (!Character.isDigit(first));
+
+                String address = "";
+
+                if(isSymbol)
+                {
+                    boolean symbolInTable = this.table.contains(symbol);
+                    if(!symbolInTable)
+                    {
+                        int dAddress = this.table.getDAddress();
+                        this.table.addEntry(symbol, dAddress);
+                        this.table.incrementDAddress();
+                    }
+
+                    address = Integer.toString(this.table.GetAddress(symbol));
+                }
+                else
+                {
+                    address = symbol;
+                }
+                instruction = this.formatA(address);
+            }
+            else if(command.equals(CommandType.C_COMMAND))
+            {
+                String comp = parser.comp();
+                String dest = parser.destination();
+                String jump = parser.jump();
+                instruction = this.formatC(comp, dest, jump);
+            }
+
+            if(!command.equals(CommandType.L_COMMAND))
+            {
+                this.write.write(instruction);
+                this.write.newLine();
+            }
+        }
+        parser.close();
+        this.write.flush();
+        this.write.close();
+    }  
+    private String formatA(String address)
+    {
+        String formatNum = this.encoder.toBinary(address);
+        return "0" + formatNum;
+    }
+    
+    private String formatC(String comp, String dest, String jump)
+    {
+        StringWriter instruction = new StringWriter();
+        instruction.append("111");
+        instruction.append(this.encoder.comp(comp));
+        instruction.append(this.encoder.dest(dest));
+        instruction.append(this.encoder.jump(jump));
+        return instruction.toString();
+    }
+}
---
+++ assembler/Main.java
@@ -0,0 +1,53 @@
+package assembler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        if(args.length == 0)
+        {
+            System.err.println("No File");
+            System.exit(1);
+        }
+        File source = new File(args[0].trim());
+		if (!source.exists()) {
+			System.err.println("Specified source file could not be found.");
+			System.exit(2);
+		}
+        String path = source.getAbsolutePath();
+        String file = source.getName();
+        int fileExtendIndex = file.lastIndexOf(".");
+        String fileNoExtend = file.substring(0, fileExtendIndex);
+        int fileNameIndex = source.getAbsolutePath().indexOf(source.getName());
+        String sourceDirect = path.substring(0, fileNameIndex);
+        String outputPath = sourceDirect + fileNoExtend + ".hack";
+        File output = new File(outputPath);
+        try 
+        {
+            if(output.exists())
+            {
+                output.delete();
+            }
+            output.createNewFile();
+
+            long startTime = System.currentTimeMillis();
+            Assembler assembler = new Assembler(source, output);
+            assembler.translate();
+
+            StringWriter status = new StringWriter();
+            status.append("Success on");
+            status.append(path);
+            status.append(" to ");
+            status.append(outputPath);
+            System.out.println(status.toString());
+        } catch (IOException e)
+        {
+            System.err.println("Error Occured");
+            System.exit(3);
+        }
+    }
+}
