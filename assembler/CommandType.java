841fb07388cb88ce29af79ae29ac7526cab15cea
Caleb <calebagaro7@gmail.com>
2023-10-18 23:09:45 -0700

    Initial

---
+++ assembler/CommandType.java
@@ -0,0 +1,6 @@
+package assembler;
+
+public enum CommandType 
+{
+    A_COMMAND, C_COMMAND, L_COMMAND;
+}
--- assembler/Parser.java
+++ assembler/Parser.java
@@ -1,8 +1,79 @@
+package assembler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Parser
 {
-    public Parser()
-    {
+    private String currentLine;
+    private String nextLine;
+    private Scanner buff;
 
+    public Parser(File file) throws IOException
+    {
+        if(file == null)
+        {
+            throw new IOException("invalid file");
+        }
+        this.buff = new Scanner(file);
+        this.currentLine = null;
+        this.nextLine = this.getNext();
+    }
+
+    private String getNext() throws IOException
+    {
+        String nextLine = "";
+        do
+        {
+            nextLine = this.buff.nextLine();
+            if(nextLine == null)
+            {
+                return null;
+            }
+        } while(nextLine.trim().isEmpty() || this.commentFlag(nextLine));
+
+        int commentIndex = nextLine.indexOf("//");
+        if(commentIndex != -1)
+        {
+            nextLine = nextLine.substring(0, commentIndex-1);
+        }
+        return nextLine;
+    }
+
+    private boolean commentFlag(String str)
+    {
+        return str.trim().startsWith("//");
+    }
+
+    @Override
+    public void finalize()
+    {
+        this.close();
+    }
+
+    public void close()
+    {
+        this.buff.close();
+    }
+
+    public boolean moreCommands()
+    {
+        return (this.nextLine != null);
+    }
+    
+    public void advance() throws IOException
+    {
+        this.currentLine = this.nextLine;
+        this.nextLine = this.getNext();
+    }
+
+    public CommandType commandType()
+    {
+        String line = this.currentLine.trim();
+        if(line.startsWith("(") && line.endsWith(")"))
+        {
+            return
+        } 
     }
 }
